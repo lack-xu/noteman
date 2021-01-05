17ca7642d8fd8953adead3880a55c8f4a0925f90
lack-xu <juluoxu@gmail.com>
2021-01-05 15:16:29 +0800

    Project Init

---
+++ backend/src/main/java/noteman/NotemanApplication.java
@@ -0,0 +1,13 @@
+package noteman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemanApplication.class, args);
+	}
+
+}
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
+  // Change build paths to make them Maven compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'target/dist',
+  assetsDir: 'static',
+
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    webpack: {
+      dir: [
+        './webpack'
+      ]
+    }
+  },
+
 }
