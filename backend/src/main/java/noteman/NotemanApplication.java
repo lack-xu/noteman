47baeba3e369cf2fff06278903bf0a485f546c64
lack-xu <juluoxu@gmail.com>
2021-01-05 18:05:00 +0800

    WHY?

--- backend/src/main/java/noteman/NotemanApplication.java
+++ backend/src/main/java/noteman/NotemanApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotemanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotemanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotemanApplication.class, args);
+    }
 
 }
