559bb32a0116e2aa34e6bb8b9a6a06adb0b912a0
lack-xu <juluoxu@gmail.com>
2021-01-13 04:04:09 +0800

    Start

---
+++ backend/noteman (1)/src/main/java/github/lackxu/noteman/NotemanApplication.java
@@ -0,0 +1,13 @@
+package github.lackxu.noteman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemanApplication.class, args);
+	}
+
+}
---
+++ backend/noteman (1)/src/test/java/github/lackxu/noteman/NotemanApplicationTests.java
@@ -0,0 +1,13 @@
+package github.lackxu.noteman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
